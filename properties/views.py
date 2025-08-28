87bb7d4a00ce821a4542e1176802124c9db0f2c1
unknown <Azegba Kizito Amandianeze>
2025-08-28 09:00:58 -0700

    modified my views.py

--- properties/views.py
+++ properties/views.py
@@ -1,7 +1,9 @@
 # properties/views.py
 from django.http import JsonResponse
 from .utils import get_all_properties
+from django.views.decorators.cache import cache_page
 
+@cache_page(60 * 15)
 def property_list(request):
     properties = get_all_properties()
     return JsonResponse({"data": properties})
