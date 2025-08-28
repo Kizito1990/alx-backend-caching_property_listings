eb3d2eaa8bc89f4a5014a4c01d63c48803b45cb8
unknown <Azegba Kizito Amandianeze>
2025-08-28 09:07:10 -0700

    added signals.py

--- properties/apps.py
+++ properties/apps.py
@@ -4,3 +4,13 @@ from django.apps import AppConfig
 class PropertiesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'properties'
+
+# properties/apps.py
+from django.apps import AppConfig
+
+class PropertiesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'properties'
+
+    def ready(self):
+        import properties.signals
