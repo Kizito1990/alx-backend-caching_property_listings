5dca2a5942c48c66b94aae319542cbeb342e4a6e
unknown <Azegba Kizito Amandianeze>
2025-08-28 08:32:08 -0700

    modified my views.py

--- properties/views.py
+++ properties/views.py
@@ -1,9 +1,13 @@
+# properties/views.py
 from django.http import JsonResponse
 from django.views.decorators.cache import cache_page
+from django.utils.decorators import method_decorator
 from .models import Property
 
 @cache_page(60 * 15)  # cache for 15 minutes
 def property_list(request):
-    properties = list(Property.objects.values())  # convert queryset to list of dicts
-    return JsonResponse(properties, safe=False)
+    properties = Property.objects.all().values("id", "title", "description", "price", "location")
+    return JsonResponse({
+        "data": list(properties)
+    })
 
